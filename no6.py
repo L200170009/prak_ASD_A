39b12644cf1871ece674d75ab8efe415e859c8d7
L200170009 <48073336+L200170009@users.noreply.github.com>
2019-03-11 23:24:17 +0700

    Add files via upload

---
+++ no6.py
@@ -0,0 +1,64 @@
+class Manusia(object):
+    """class 'Manusia' dengan inisiasi 'nama'"""
+    keadaan = "lapar"
+    def __init__(self, nama):
+        self.nama = nama
+    def ucapkanSalam(self):
+        print("Salam, namaku ",self.nama)
+    def makan(self, s):
+        print("Saya baru saja makan ", s)
+        self.keadaan = 'kenyang'
+    def olahraga(self, k):
+        print("Saya baru saja latihan ", k)
+        self.keadaan = 'lapar'
+    def mengalikanDenganDua(self, n):
+        return n * 2
+
+class Mahasiswa(Manusia):
+    """Class Mahasiswa yang dibangun dari class Manusia."""
+    def __init__(self, nama, NIM, kota, us):
+        """Metode inisiasi ini menutupi metode inisiasi di class Manusia"""
+        self.nama = nama
+        self.NIM = NIM
+        self.kotaTinggal = kota
+        self.uangSaku = us
+    def __str__(self):
+        s = self.nama + ', NIM ' + str(self.NIM) \
+            + '. Tinggal di ' + self.kotaTinggal \
+            + '. Uang saku Rp ' + str(self.uangSaku) \
+            + ' tiap bulannya.'
+        return s
+    def ambilNama(self):
+        return self.nama
+    def ambilNIM(self):
+        return self.NIM
+    def ambilUangSaku(self):
+        return self.uangSaku
+    def makan(self, e):
+        """Metode ini menutupi metode 'makan'-nya class manusia.
+           Mahasiswa kalau makan sambil belajar."""
+        print("Saya baru saja makan", s, "Sambil belajar.")
+        self.keadaan = 'kenyang'
+
+class SiswaSMA(Manusia):
+    """Membuat class SiswaSMA dari class Manusia"""
+    def __init__(self,nama,no_induk,kelas,alamat):
+        """Metode inisialisasi ini menutupi metode inisiasi di class Manusia."""
+        self.nama = nama
+        self.nisn = NISN
+        self.kelas = kelas
+        self.alamat = alamat
+    def __str__(self):
+        a = "Nama       : " + (self.nama)     +'\n'
+            "NISN       : " + str(self.nisn)+'\n'
+            "Kelas      : " + str(self.kelas) +'\n'
+            "Alamat     : " + (self.alamat)
+        print (a)
+    def ambilNama(self):
+        print (self.nama)
+    def ambilNoInduk(self):
+        print (self.nisn)
+    def ambilKelas(self):
+        print (self.kelas)
+    def ambilAlamat(self):
+        print (self.alamat)
---
+++ no7.py
@@ -0,0 +1,30 @@
+class MhsTIF(Mahasiswa):
+    """Class MhsTIF yang dibangun dari class Mahasiswa"""
+    def katakanpy(self):
+        print('Python is cool.')
+
+##class Manusia:
+##    1. nama
+##    2. keadaan
+##    3. ucapkanSalam
+##    4. makan
+##    5. olahraga
+##    6. mengalikanDenganDua
+
+## class Mahasiswa:
+##    1. NIM
+##    2. kotaTinggal
+##    3. uangsaku
+##    4. ambilNama
+##    5. ambilNIM
+##    6. ambilUangSaku
+##    7. makan
+##    8. ambilKotaTinggal
+##    9. perbaruiKotaTinggal
+##    10. tambahUangSaku
+##    11. listKuliah
+##    12. ambilKuliah
+##    13. hapusKuliah
+
+##class MhsTIF:
+##    1. katakanpy
